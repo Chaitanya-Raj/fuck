a8bbe818fcaddbc6b5a9bafa21ca696d5bfb18d5
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-05-23 16:56:45 +0530

    Updates

---
+++ precache-manifest.c64f83c2a32b40d130ec848c35e8a671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c18ed8838409b02222f40420ddb272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "218d41afe09ded5fee73",
+    "url": "/static/css/main.4f8f9fdd.chunk.css"
+  },
+  {
+    "revision": "e1defb4897af9b5a2c99",
+    "url": "/static/js/2.b1d6806a.chunk.js"
+  },
+  {
+    "revision": "d4c934bb64954773e555a37fb3525224",
+    "url": "/static/js/2.b1d6806a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218d41afe09ded5fee73",
+    "url": "/static/js/main.253f11bf.chunk.js"
+  },
+  {
+    "revision": "21ad0d7e685e7924444f",
+    "url": "/static/js/runtime-main.2f0c6037.js"
+  }
+]);
